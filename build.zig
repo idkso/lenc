f7a42c0945839965b235ed4987c5a12e9700b636
lenny <jakkleugos@gmail.com>
2022-10-20 23:36:15 -0700

    changed exe name to lenc :lenny:

--- build.zig
+++ build.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 pub fn build(b: *std.build.Builder) void {
     const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
-    const exe = b.addExecutable("enc", "src/main.zig");
+    const exe = b.addExecutable("lenc", "src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
     exe.addPackagePath("clap", "lib/zig-clap/clap.zig");
