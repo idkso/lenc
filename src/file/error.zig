8101dbff4da9fb1f0730256c4fc634ad27082734
lenny <jakkleugos@gmail.com>
2022-10-24 19:25:08 -0700

    oops used enum literals instead of error literals

--- src/file/error.zig
+++ src/file/error.zig
@@ -3,8 +3,8 @@ const util = @import("../util.zig");
 
 pub fn err(e: anyerror) noreturn {
     switch (e) {
-        .AuthenticationFailed => util.err("authentication failed, please check the keys used"),
-        .OutOfMemory => util.err("out of memory, try turning the chunk size down"),
+        error.AuthenticationFailed => util.err("authentication failed, please check the keys used"),
+        error.OutOfMemory => util.err("out of memory, try turning the chunk size down"),
         else => util.err("unexpected error"),
     }
 }
